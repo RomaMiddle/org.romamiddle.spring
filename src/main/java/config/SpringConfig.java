32e298ca347ccef8384763865dce2c40f38a5837
RomaMiddle <batichta@yandex.ru>
2022-03-15 15:34:16 +0400

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/config/SpringConfig.java
@@ -0,0 +1,19 @@
+package config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+@Configuration
+@ComponentScan("ru.romamiddle.spring")
+@EnableWebMvc
+public class SpringConfig implements WebMvcConfigurer {
